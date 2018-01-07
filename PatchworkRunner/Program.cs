291b6fbadbe90d9c695e4342eccee55952f2af45
danzel <danzel@localhost.geek.nz>
2018-01-07 14:49:30 +1300

    Test program for visualising placement strategy results.

--- PatchworkRunner/Program.cs
+++ PatchworkRunner/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using PatchworkSim;
-using PatchworkSim.AI;
 using PatchworkSim.AI.MoveMakers;
 using PatchworkSim.AI.PlacementFinders;
+using PatchworkSim.AI.PlacementFinders.PlacementStrategies;
 using PatchworkSim.Loggers;
 
 namespace PatchworkRunner
@@ -10,6 +10,57 @@ namespace PatchworkRunner
 	class Program
 	{
 		static void Main(string[] args)
+		{
+			ComparePlacementStrategies();
+			//WatchAGame();
+		}
+
+		private static void ComparePlacementStrategies()
+		{
+			//State only exists so we can use the logger
+			var state = new SimulationState(SimulationHelpers.GetRandomPieces(1), 0);
+			var logger = new ConsoleLogger(state);
+
+			var strategies = new[]
+			{
+				FirstPossiblePlacementStrategy.Instance,
+				FirstPossiblePlacementStrategy.Instance
+			};
+
+			var pieces = SimulationHelpers.GetRandomPieces(1);
+
+			var placed = new[] { 0, 0 };
+			var stillPlacing = new[] { true, true };
+
+			for (var index = 0; index < pieces.Length; index++)
+			{
+				var pieceIndex = pieces[index];
+				var piece = PieceDefinition.AllPieceDefinitions[pieceIndex];
+				Console.WriteLine($"Placing piece {index} - {piece.Name}");
+
+				for (var i = 0; i < 2; i++)
+				{
+					if (!stillPlacing[i])
+						continue;
+					if (strategies[i].TryPlacePiece(state.PlayerBoardState[i], piece, out var bitmap, out var x, out var y))
+					{
+						placed[i]++;
+						BitmapOps.Place(state.PlayerBoardState[i], bitmap, x, y);
+					}
+					else
+					{
+						stillPlacing[i] = false;
+					}
+				}
+				logger.PrintBoards(true);
+				if (!stillPlacing[0] && !stillPlacing[1])
+					break;
+			}
+
+			Console.ReadLine();
+		}
+
+		private static void WatchAGame()
 		{
 			var state = new SimulationState(SimulationHelpers.GetRandomPieces(1), 0);
 			var runner = new SimulationRunner(state, new PlayerDecisionMaker(BuyFirstPossibleMoveMaker.Instance, PlacementMaker.FirstPossibleInstance), new PlayerDecisionMaker(BuyFirstPossibleMoveMaker.Instance, PlacementMaker.FirstPossibleInstance));
