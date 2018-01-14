be33814cf2e576bc851a98dc2a5775eceafbc2e9
danzel <danzel@localhost.geek.nz>
2018-01-14 16:53:43 +1300

    Add another bad placement finder

---
+++ PatchworkSim.AI/PlacementFinders/PlacementStrategies/SmallestBoundingBoxPlacementStrategy.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace PatchworkSim.AI.PlacementFinders.PlacementStrategies
+{
+	public class SmallestBoundingBoxPlacementStrategy : IPlacementStrategy
+	{
+		public static readonly SmallestBoundingBoxPlacementStrategy Instance = new SmallestBoundingBoxPlacementStrategy();
+
+		public string Name => "SmallestBoundingBox";
+
+		private SmallestBoundingBoxPlacementStrategy()
+		{
+		}
+
+		public bool TryPlacePiece(BoardState board, PieceDefinition piece, out PieceBitmap resultBitmap, out int resultX, out int resultY)
+		{
+			int smallestSize = BoardState.Height * BoardState.Width + 1;
+
+			resultBitmap = null;
+			resultX = 0;
+			resultY = 0;
+
+
+			//TODO: If we weren't a singleton we could track our used Width/Height
+			int currentHeight = CalcUsedHeight(ref board);
+			int currentWidth = CalcUsedWidth(ref board);
+			Console.WriteLine($"Current {currentWidth}x{currentHeight}");
+
+			for (var x = 0; x < BoardState.Width; x++)
+			{
+				for (var y = 0; y < BoardState.Height; y++)
+				{
+					foreach (var bitmap in piece.PossibleOrientations)
+					{
+						var size = Math.Max(currentWidth, x + bitmap.Width) * Math.Max(currentHeight, y + bitmap.Height);
+
+						if (size < smallestSize && board.CanPlace(bitmap, x, y))
+						{
+							smallestSize = size;
+							resultBitmap = bitmap;
+							resultX = x;
+							resultY = y;
+						}
+					}
+				}
+			}
+
+			return resultBitmap != null;
+		}
+
+		private static int CalcUsedHeight(ref BoardState board)
+		{
+			for (var y = BoardState.Height - 1; y >= 0; y--)
+			{
+				for (var x = 0; x < BoardState.Width; x++)
+				{
+					if (board[x, y])
+						return y + 1;
+				}
+			}
+
+			return 0;
+		}
+
+		private static int CalcUsedWidth(ref BoardState board)
+		{
+			for (var x = BoardState.Width - 1; x >= 0; x--)
+			{
+				for (var y = 0; y < BoardState.Width; y++)
+				{
+					if (board[x, y])
+						return x + 1;
+				}
+			}
+
+			return 0;
+		}
+	}
+}
--- PathworkSim.Test/PlacementStrategyTests.cs
+++ PathworkSim.Test/PlacementStrategyTests.cs
@@ -58,5 +58,32 @@ namespace PathworkSim.Test
 			//Hopefully this is at least as many places as FirstPossible
 			Assert.Equal(13, placed);
 		}
+
+		[Fact]
+		public void SmallestBoundingBox()
+		{
+			var pieces = SimulationHelpers.GetRandomPieces(1);
+			int placed = 0;
+
+			var board = new BoardState();
+
+			foreach (var piece in pieces)
+			{
+				if (SmallestBoundingBoxPlacementStrategy.Instance.TryPlacePiece(board, PieceDefinition.AllPieceDefinitions[piece], out var bitmap, out var x, out var y))
+				{
+					placed++;
+					board.Place(bitmap, x, y);
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			//This number totally relies on the order of pieces in the array.
+			//Update it if the ordering changes, the extact amount isn't important
+			//Hopefully this is at least as many places as FirstPossible
+			Assert.Equal(12, placed);
+		}
 	}
 }
