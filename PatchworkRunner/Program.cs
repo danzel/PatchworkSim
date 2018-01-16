9cf0e830a75f80790221f8bce253eb16bf7776f8
danzel <danzel@localhost.geek.nz>
2018-01-16 20:27:48 +1300

    Let PatchworkRunner (visual placement tester) run more than just 2 strategies at a time

--- PatchworkRunner/Program.cs
+++ PatchworkRunner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using PatchworkSim;
 using PatchworkSim.AI.MoveMakers;
 using PatchworkSim.AI.PlacementFinders;
@@ -18,19 +19,29 @@ namespace PatchworkRunner
 		private static void ComparePlacementStrategies()
 		{
 			//State only exists so we can use the logger
-			var state = new SimulationState(SimulationHelpers.GetRandomPieces(1), 0);
-			var logger = new ConsoleLogger(state);
+			//var state = new SimulationState(SimulationHelpers.GetRandomPieces(2), 0);
+			//var logger = new ConsoleLogger(state);
 
 			var strategies = new IPlacementStrategy[]
 			{
-				FirstPossiblePlacementStrategy.Instance,
-				SimpleClosestToWallAndCornerStrategy.Instance
+				//FirstPossiblePlacementStrategy.Instance,
+				//SimpleClosestToWallAndCornerStrategy.Instance,
+				//SmallestBoundingBoxPlacementStrategy.Instance
+				ClosestToCornerLeastHolesTieBreakerPlacementStrategy.Instance,
+				NextToPieceEdgeLeastHolesTieBreakerPlacementStrategy.Instance,
+				TightPlacementStrategy.InstanceDoubler,
+				TightPlacementStrategy.InstanceIncrement
+				//new NextToPieceEdgeAllowedHolesTieBreakerPlacementStrategy(15)
 			};
 
-			var pieces = SimulationHelpers.GetRandomPieces(1);
 
-			var placed = new[] { 0, 0 };
-			var stillPlacing = new[] { true, true };
+			var pieces = SimulationHelpers.GetRandomPieces(2);
+
+			var oldBoards = new BoardState[strategies.Length];
+			var boards = new BoardState[strategies.Length];
+			var placed = new int[strategies.Length];
+			var stillPlacing = strategies.Select(s => true).ToArray();
+			int stillPlacingCount = strategies.Length;
 
 			for (var index = 0; index < pieces.Count; index++)
 			{
@@ -38,23 +49,28 @@ namespace PatchworkRunner
 				var piece = PieceDefinition.AllPieceDefinitions[pieceIndex];
 				Console.WriteLine($"Placing piece {index} - {piece.Name}");
 
-				for (var i = 0; i < 2; i++)
+				for (var i = 0; i < strategies.Length; i++)
 				{
 					if (!stillPlacing[i])
 						continue;
-					if (strategies[i].TryPlacePiece(state.PlayerBoardState[i], piece, out var bitmap, out var x, out var y))
+					if (strategies[i].TryPlacePiece(boards[i], piece, out var bitmap, out var x, out var y))
 					{
 						placed[i]++;
-						state.PlayerBoardState[i].Place(bitmap, x, y);
+						boards[i].Place(bitmap, x, y);
 					}
 					else
 					{
 						stillPlacing[i] = false;
+						stillPlacingCount--;
 					}
 				}
-				logger.PrintBoards(true);
-				if (!stillPlacing[0] && !stillPlacing[1])
+
+				ConsoleLogger.PrintBoardsDiff(oldBoards, boards);
+				if (stillPlacingCount == 0)
 					break;
+
+				for (var i = 0; i < strategies.Length; i++)
+					oldBoards[i] = boards[i];
 			}
 
 			Console.ReadLine();
