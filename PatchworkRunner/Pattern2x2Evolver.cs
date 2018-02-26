e4f399f30e1e9919d9c15b628388f656bd9510cf
dave <danzel@localhost.geek.nz>
2018-02-26 15:55:58 +1300

    An evolver for the Pattern2x2 evaluator.
    Refs #24

---
+++ PatchworkRunner/Pattern2x2Evolver.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using PatchworkSim;
+using PatchworkSim.AI.MoveMakers;
+using PatchworkSim.AI.PlacementFinders.PlacementStrategies.BoardEvaluators;
+using PatchworkSim.AI.PlacementFinders.PlacementStrategies.NoLookahead;
+
+namespace PatchworkRunner
+{
+	class Pattern2x2Evolver
+	{
+		private const int PopulationSize = 30;
+		private readonly Random _random = new Random();
+		List<PopulationMember> _population;
+		const int MaxGeneration = 10_000;
+
+		private const int MinValue = -100;
+		private const int MaxValue = 100;
+
+		private void GenerateInitialPopulation()
+		{
+			_population = new List<PopulationMember>();
+
+			for (var i = _population.Count; i < PopulationSize; i++)
+			{
+				var populationMember = new PopulationMember(CreateRandomGene());
+				EvaluateFitness(populationMember);
+				_population.Add(populationMember);
+			}
+		}
+
+		public void Run()
+		{
+			var generation = 0;
+			var lastBestFitness = 0;
+			GenerateInitialPopulation();
+
+			while (true)
+			{
+				_population.Sort();
+
+				if (generation % 100 == 0)
+					Console.WriteLine($"Generation {generation}. Fitness Range: {_population[0].Fitness} -- {_population[PopulationSize - 1].Fitness}");
+
+				if (_population[0].Fitness > lastBestFitness)
+				{
+					lastBestFitness = _population[0].Fitness;
+					Console.WriteLine(_population[0].Strategy.Name);
+				}
+
+				//Do the genetic thing.
+				//Replace the quarter with new versions based on the best ones
+				int fitnessSum = 0;
+				for (var i = 0; i < (3 * PopulationSize / 4); i++)
+				{
+					var p = _population[i];
+					fitnessSum += p.Fitness;
+				}
+
+				for (var i = 0; i < PopulationSize / 4; i++)
+				{
+					var index0 = randomPick(fitnessSum);
+					var index1 = randomPick(fitnessSum);
+
+					var parent0 = _population[index0];
+					var parent1 = _population[index1];
+
+					//Crossover and Mutation
+					var target = _population[PopulationSize - 1 - i];
+					var gene = target.Gene;
+
+					for (var j = 0; j < PopulationMember.GeneSize; j++)
+					{
+						//TODO: Could use random more efficiently
+						if (_random.NextDouble() < 0.5)
+							gene[j] = (_random.NextDouble() < 0.5 ? parent0 : parent1).Gene[j]; //Crossover
+						else
+							gene[j] = Clamp(gene[j] + _random.Next(MinValue / 5, 1 + MaxValue / 5)); //Mutation
+					}
+
+					target.CreateMoveMaker();
+					EvaluateFitness(_population[PopulationSize - 1 - i]);
+				}
+
+				generation++;
+
+				if (generation == MaxGeneration)
+				{
+					File.AppendAllLines($"best-{lastBestFitness}-{DateTimeOffset.UtcNow.Ticks}.txt", new[] { $"{lastBestFitness} {_population[0].Strategy.Name}" });
+					generation = 0;
+					lastBestFitness = 0;
+					GenerateInitialPopulation();
+					//return;
+				}
+			}
+		}
+
+		private int randomPick(int fitnessSum)
+		{
+			var rand = _random.Next(0, fitnessSum);
+
+			for (var i = 0; i < PopulationSize; i++)
+			{
+				rand -= _population[i].Fitness;
+				if (rand <= 0)
+					return i;
+			}
+
+			throw new Exception();
+		}
+
+		private void EvaluateFitness(PopulationMember populationMember)
+		{
+			populationMember.Fitness = 1 + CalculateChallengerWinsFrom100(populationMember.Strategy);
+		}
+
+		private int[] CreateRandomGene()
+		{
+			var res = new int[PopulationMember.GeneSize];
+			for (var i = 0; i < PopulationMember.GeneSize; i++)
+				res[i] = _random.Next(MinValue, 1 + MaxValue);
+			return res;
+		}
+
+		private const int GameCount = 200;
+
+		private static readonly List<PieceDefinition>[] RandomPiecesCache = Enumerable.Range(0, GameCount).Select(i => SimulationHelpers.GetRandomPieces(i).Select(p => PieceDefinition.AllPieceDefinitions[p]).ToList()).ToArray();
+
+		/// <summary>
+		/// Returns the amount of wins the challenger got (from 100)
+		/// </summary>
+		private static int CalculateChallengerWinsFrom100(NoLookaheadStrategy strategy)
+		{
+			int totalPlacements = 0;
+
+			PieceCollection empty = new PieceCollection();
+
+			Parallel.For(0, GameCount, new ParallelOptions { MaxDegreeOfParallelism = 8 }, (i) =>
+			{
+				var pieces = RandomPiecesCache[i];
+				var placements = 0;
+
+				var board = new BoardState();
+				var pieceIndex = 0;
+				while (strategy.TryPlacePiece(board, pieces[pieceIndex], in empty, 0, out var bitmap, out var x, out var y))
+				{
+					board.Place(bitmap, x, y);
+
+					pieceIndex++;
+				}
+
+				Interlocked.Add(ref totalPlacements, pieceIndex);
+			});
+
+			return totalPlacements;
+		}
+
+		/// <summary>
+		/// Clamp between MinValue and MaxValue inclusive
+		/// </summary>
+		private int Clamp(int value)
+		{
+			return value < MinValue ? MinValue : value > MaxValue ? MaxValue : value;
+		}
+
+
+		class PopulationMember : IComparable<PopulationMember>
+		{
+			public const int GeneSize = 16;
+
+			public int[] Gene;
+			public BestEvaluatorStrategy Strategy;
+
+			public int Fitness;
+
+			public PopulationMember(int[] gene)
+			{
+				Gene = gene;
+				CreateMoveMaker();
+			}
+
+			public void CreateMoveMaker()
+			{
+				//TODO: Garbage generation here
+				Strategy = new BestEvaluatorStrategy(new TuneablePattern2x2BoardEvaluator(Gene));
+			}
+
+			public int CompareTo(PopulationMember other)
+			{
+				if (ReferenceEquals(this, other)) return 0;
+				if (ReferenceEquals(null, other)) return 1;
+				return other.Fitness - Fitness; //Fitness.CompareTo(other.Fitness);
+			}
+		}
+	}
+}
---
+++ PatchworkSim.AI/PlacementFinders/PlacementStrategies/BoardEvaluators/TuneablePattern2x2BoardEvaluator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace PatchworkSim.AI.PlacementFinders.PlacementStrategies.BoardEvaluators
+{
+	public class TuneablePattern2x2BoardEvaluator : IBoardEvaluator
+	{
+		private readonly int[] _weights;
+
+		public string Name
+		{
+			get
+			{
+				var sb = new StringBuilder();
+				sb.Append("TuneablePattern2x2BoardEvaluator(");
+
+				for (var i = 0; i < _weights.Length; i++)
+				{
+					if (i > 0)
+						sb.Append('|');
+					sb.Append(_weights[i]);
+				}
+
+				sb.Append(')');
+				return sb.ToString();
+			}
+		}
+
+		public TuneablePattern2x2BoardEvaluator(int[] weights)
+		{
+			if (weights.Length != 16)
+				throw new Exception("Expected 16 weights");
+			_weights = weights;
+		}
+
+		public void BeginEvaluation(BoardState currentBoard)
+		{
+		}
+
+		public int Evaluate(in BoardState board, int minX, int maxX, int minY, int maxY)
+		{
+			int points = 0;
+
+			for (var x = minX - 1; x < maxX; x++)
+			{
+				var bottomLeft = Read(in board, x, minY - 1);
+				var bottomRight = Read(in board, x + 1, minY - 1);
+
+				for (var y = minY - 1; y < maxY; y++)
+				{
+					var topLeft = bottomLeft;
+					var topRight = bottomRight;
+
+					bottomLeft = Read(in board, x, y + 1);
+					bottomRight = Read(in board, x + 1, y + 1);
+
+					var index = (topLeft ? 0b1000 : 0) | (topRight ? 0b0100 : 0) | (bottomLeft ? 0b0010 : 0) | (bottomRight ? 0b0001 : 0);
+
+					points += _weights[index];
+				}
+			}
+
+			return points;
+		}
+
+		private bool Read(in BoardState board, int x, int y)
+		{
+			if (x < 0 || y < 0 || x >= BoardState.Width || y >= BoardState.Height)
+				return true;
+			return board[x, y];
+		}
+	}
+}
