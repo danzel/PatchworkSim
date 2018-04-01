e0b8cc2f9d6e9e5046a41a75b7ec0f6e2c830921
danzel <danzel@localhost.geek.nz>
2018-04-02 08:09:53 +1200

    BulkBoardEvaluator - Batch up evaluations when they exceed (hardcoded) GPU memory size

--- PatchworkSim.AI.CNTK/BulkBoardEvaluator.cs
+++ PatchworkSim.AI.CNTK/BulkBoardEvaluator.cs
@@ -38,38 +38,42 @@ namespace PatchworkSim.AI.CNTK
 		{
 			//https://github.com/Microsoft/CNTK/blob/06ee7b83a5a95457f2e6e1fd8a452f28dc47ebd0/Examples/Evaluation/CNTKLibraryCSEvalCPUOnlyExamples/CNTKLibraryCSEvalExamples.cs#L105
 			// EvaluationBatchOfImages
+			const int maxBatchSize = 20000; //TODO: Depends on GPU memory size
 
-			float[] vals = new float[BoardState.Width * BoardState.Height * boards.Count];
-
-			for (var i = 0; i < boards.Count; i++)
+			for (var start = 0; start < boards.Count; start += maxBatchSize)
 			{
-				CNTKHelpers.CopyBoardToArray(boards[i].Board, vals, i * BoardState.Width * BoardState.Height);
+				var count = Math.Min(maxBatchSize, boards.Count - start);
+
+				float[] vals = new float[BoardState.Width * BoardState.Height * count];
+
+				for (var i = 0; i < count; i++)
+				{
+					CNTKHelpers.CopyBoardToArray(boards[start + i].Board, vals, i * BoardState.Width * BoardState.Height);
+				}
+
+				var inputDataMap = new Dictionary<Variable, Value>();
+				var inputVal = Value.CreateBatch(_inputShape, vals, _device);
+				inputDataMap.Add(_inputVar, inputVal);
+
+				var outputDataMap = new Dictionary<Variable, Value>();
+				outputDataMap.Add(_outputVar, null);
+
+				_modelFunc.Evaluate(inputDataMap, outputDataMap, _device);
+
+				var outputVal = outputDataMap[_outputVar];
+				var outputData = outputVal.GetDenseData<float>(_outputVar);
+
+				for (var b = 0; b < count; b++)
+				{
+					var ourData = outputData[b];
+
+					boards[start + b].SetScore(ourData[1]);
+				}
+
+				//https://github.com/Microsoft/CNTK/issues/2954
+				inputVal.Erase();
+				outputVal.Erase();
 			}
-
-
-			var inputDataMap = new Dictionary<Variable, Value>();
-			var inputVal = Value.CreateBatch(_inputShape, vals, _device);
-			inputDataMap.Add(_inputVar, inputVal);
-
-			var outputDataMap = new Dictionary<Variable, Value>();
-			outputDataMap.Add(_outputVar, null);
-
-			_modelFunc.Evaluate(inputDataMap, outputDataMap, _device);
-
-			var outputVal = outputDataMap[_outputVar];
-			var outputData = outputVal.GetDenseData<float>(_outputVar);
-
-			for (var b = 0; b < boards.Count; b++)
-			{
-				var ourData = outputData[b];
-
-				boards[b].SetScore(ourData[1]);
-			}
-
-
-			//https://github.com/Microsoft/CNTK/issues/2954
-			inputVal.Erase();
-			outputVal.Erase();
 		}
 	}
 }
